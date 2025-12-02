48df5290c61951786a6f6ffba5ec673d0a4e8b66
Kamlesh <kamleshchandela51@gmail.com>
2025-12-02 12:29:12 +0530

    c language test ans.

--- clanguage/Q3.c
+++ clanguage/Q3.c
@@ -5,7 +5,7 @@ int main() {
     int arr[7] = { 20, 32, 6, 7, 22, 47, 21};
     int max = arr[0];
     int min = arr[0];
-    for(int i = 0; i < sizeof(arr)/4 - 1 ; i++){
+    for(int i = 0; i < sizeof(arr)/4 ; i++){
         if(max < arr[i]){
             max = arr[i];
         }
